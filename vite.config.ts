71ab682a5a22dcfc120e7f5060b4017b39714f6b
shreydalal318 <shreydalal53@gmail.com>
2026-01-09 15:56:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/navkarhosiery/', // ❗ replace with your repo name
+  base: '/', // ❗ replace with your repo name
 })
