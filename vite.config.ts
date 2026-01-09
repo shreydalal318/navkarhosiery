434cd6db5bfaaf15438ea6026266db3ac4940006
shreydalal318 <shreydalal53@gmail.com>
2026-01-09 16:04:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ❗ replace with your repo name
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/navkarhosiery/', // repo name
 })
